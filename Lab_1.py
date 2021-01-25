a31b2c854e5882a201174e4fe21cabf9a1d6c06d
sagdog98 <36053971+sagdog98@users.noreply.github.com>
2021-01-25 13:29:05 -0800

    Update Lab_1.py

--- Lab_1.py
+++ Lab_1.py
@@ -1,37 +1,50 @@
+# A list of numbers that will be used for testing our programs
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
+# Question 1: Create a function called even, which takes in an integer as input and returns true if the input is even, and false otherwise
+def even(num):
+    # Provide your code here
+    return True if num % 2 == 0 else False
 
-# Give a prefix representation of a proposition, of the form prop = ('OR', True, False)
-# Write a function named evaluate, which will evaluate the proposition
-def evaluate(formula):
-    if (formula[0] == 'AND'):
-        return AND(formula[1], formula[2])
-    elif (formula[0] == 'OR'):
-        return OR(formula[1], formula[2])
-    elif (formula[0] == 'IF'):
-        return IF(formula[1], formula[2])
-    elif (formula[0] == 'IFF'):
-        return IFF(formula[1], formula[2])
-    elif (formula[0] == 'NOT'):
-        return NOT(formula[1])
-    
-   
+print("even(1):\t\t\t\t", even(1)) # should return False
+print( "even(2):\t\t\t\t", even(2)) # shourd return True
+print()
 
-# Create a new version of your evaluate function, named evaluate_r, which also takes in a formula, but it is able to evaluate composite formulae, such as ('OR', ('NOT', True), ('AND', True, False))
+# Question 2: Create a function called odd, which takes in an integer as input and returns true if the input is odd, and false otherwise
+def odd(num):
+    # Provide your code here
+    return False if num % 2 == 0 else True
 
-def evaluate_r(formula):
-    if (len(formula) < 3):
-        conversion = list(formula)
-        conversion.append(False)
-        formula = tuple(conversion)
-    if (type(formula[1]) is not bool):
-        p = evaluate_r(formula[1])
+print("odd(1):\t\t\t\t\t", odd(1)) # should return True
+print("odd(2):\t\t\t\t\t", odd(2)) # shourd return False
+print()
+
+# Question 3: Given a list of integers, create a function called count_odd, which takes in a list and counts the number of odd. Your function should employ a divide and conquer approach. Select an appropriate base case and implement a recursive step.
+def count_odd(list):
+    # Provide your code here
+    return 0 if len(list) == 0 else (list[0] % 2 + count_odd(list[1:]))
+
+print("count_odd([1, 2, 3, 4, 5, 6, 7, 8]):\t", count_odd(numbers)) # should return 4
+print()
+
+# Question 4: Given a list of integers, use a divide and conquer approach to create a function named reverse, which takes in a list and returns the list in reverse order.
+def reverse(list):
+    # Provide your code here
+    if len(list) == 0:
+        return []
     else:
-        p = formula
-    if (type(formula[2]) is not bool):
-        q = evaluate_r(formula[2])
-    else: 
-        q = formula[2]
+        return reverse(list[1:]) + list[:1] if list else []
 
-    return evaluate(((formula[0], p, q)))
+print("reverse([1, 2, 3, 4, 5, 6, 7, 8]):\t", reverse(numbers)) # should return [8, 7, 6, 5, 4, 3, 2, 1]
+print()
 
+# Question 5: Given two sorted lists, it is possible to merge them into one sorted lists in an efficient way. Design and implement a divide and conquer algorithm to merge two sorted lists.
+def merge(list1, list2):
+    # Provide your code here
+    if list1 and list2:
+        if list1[0] > list2[0]:
+            list1, list2 = list2, list1
+        return [list1[0]] + merge(list1[1:], list2)
+    return list1 + list2
 
+print("merge([1, 3, 5, 7], [2, 4, 6, 8]):\t", merge([1, 3, 5, 7], [2, 4, 6, 8])) # should return [1, 2, 3, 4, 5, 6, 7, 8]
